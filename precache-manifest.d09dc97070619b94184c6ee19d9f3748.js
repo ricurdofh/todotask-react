b33178c7d85350adaac5158231b2622a5bb74140
ricurdofh <ricardo.servitechs@gmail.com>
2019-12-09 05:57:24 -0400

    Updates

---
+++ precache-manifest.d09dc97070619b94184c6ee19d9f3748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907574a524a21b180167f8096ee31b42",
+    "url": "/todotask-react/index.html"
+  },
+  {
+    "revision": "94d264cc7146b26efa4f",
+    "url": "/todotask-react/static/css/main.715488c1.chunk.css"
+  },
+  {
+    "revision": "06c2c0309fc9f0457b68",
+    "url": "/todotask-react/static/js/2.85065e0c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todotask-react/static/js/2.85065e0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94d264cc7146b26efa4f",
+    "url": "/todotask-react/static/js/main.aebc8b8a.chunk.js"
+  },
+  {
+    "revision": "76a684066d746ba4ab0d",
+    "url": "/todotask-react/static/js/runtime-main.495cf0c3.js"
+  }
+]);
